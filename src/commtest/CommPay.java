0a5c03ff85984eb8add2f70d81061fe2061792e3
Armando89 <43739205+Armando89@users.noreply.github.com>
2020-05-01 22:14:18 -0500

    Updated class "CommPay"

--- src/commtest/CommPay.java
+++ src/commtest/CommPay.java
@@ -7,5 +7,33 @@ package commtest;
 
 public class CommPay {
     
+    //instance variables(are automatically initiazed)
+    private String firstName, lastName;
+    private int itemsSold;
+    private double itemsCost, wPaycheck, totalAmount, totalComm;
+    private final double COMMISSION = .10;
+    private final double BASE_PAY = 200;
     
+    //setter(method) that sets user input in the objects 
+    public void setValues(String firstName, String lastName, int itemsSold, double itemsCost)
+    {
+        //storing the input for each variable
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.itemsSold = itemsSold;
+        this.itemsCost = itemsCost;
+    }
+    
+    public void WklyPaycheck()
+    {
+        totalAmount = (double)itemsSold * itemsCost;
+        totalComm = totalAmount * COMMISSION;
+        wPaycheck = totalComm + BASE_PAY;
+    }
+            
+    //
+    public double getWklyPaycheck()
+    {
+        return wPaycheck;
+    }
 }
